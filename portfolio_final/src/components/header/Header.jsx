624212912dc4a1008600d0bcac1563c53135e6a4
monari1 <francisnyasinga@gmail.com>
2023-12-20 00:19:24 +0300

    today we have done the first component and set up

---
+++ portfolio_final/src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/img1.jpg'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>
+          Hello, I'm 
+        </h5>
+        <h1>
+          FRANCIS MONARI
+        </h1>
+        <h5 className='text-light'>
+          Fullstack Developer
+          
+        </h5>
+        <CTA />
+        <div className='me'>
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className='scroll_down'Scroll Down></a>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ portfolio_final/src/components/header/HeaderSocial.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderSocial = () => {
+  return (
+    <div>HeaderSocial</div>
+  )
+}
+
+export default HeaderSocial
