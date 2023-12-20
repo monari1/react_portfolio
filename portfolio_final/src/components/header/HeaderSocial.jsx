ea492a1bb06abd76ba7517097330483ddbdb43c3
monari1 <francisnyasinga@gmail.com>
2023-12-20 23:04:08 +0300

    few changes to jsx

--- portfolio_final/src/components/header/HeaderSocial.jsx
+++ portfolio_final/src/components/header/HeaderSocial.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const HeaderSocial = () => {
   return (
-    <div>HeaderSocial</div>
+    <div className='header_socials'>
+        <a href="https://linkedin.com" target='_blank'></a>
+        <a href="https://github.com" target='_blank'></a>
+        <a href="https://twitter.com" target='_blank'></a>
+
+
+    </div>
   )
 }
 
