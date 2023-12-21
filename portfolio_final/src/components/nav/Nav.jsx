cdc4ca4e4c88a654d0dc6327daf098dedd916fde
monari1 <francisnyasinga@gmail.com>
2023-12-21 23:21:22 +0300

    nav completed

--- portfolio_final/src/components/nav/Nav.jsx
+++ portfolio_final/src/components/nav/Nav.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './Nav.css'
+import { CiHome } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
+import { LuBookMinus } from "react-icons/lu";
+import { RiServiceLine } from "react-icons/ri";
+import { AiOutlineMessage } from "react-icons/ai";
+import { useState } from 'react';
+
+
+
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
-  )
+<nav>
+  <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><CiHome /></a>
+  <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><CiUser /></a>
+  <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><LuBookMinus /></a>
+  <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+  <a href="#contacts" onClick={() => setActiveNav('#contacts')} className={activeNav === '#contacts' ? 'active' : ''}><AiOutlineMessage /></a>
+ 
+</nav>  )
 }
 
 export default Nav
