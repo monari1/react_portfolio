6444be405b99681fae198b9ef715df0a5a5bf369
monari1 <francisnyasinga@gmail.com>
2023-12-21 22:29:48 +0300

    done with header

--- portfolio_final/src/components/header/Header.jsx
+++ portfolio_final/src/components/header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
         <div className='me'>
           <img src={ME} alt="me" />
         </div>
-        <a href="#contact" className='scroll_down'Scroll Down></a>
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
 
       </div>
     </header>
