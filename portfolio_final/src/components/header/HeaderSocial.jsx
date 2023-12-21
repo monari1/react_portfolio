378a3c5d12d6cb8c925d10967b5fe03ba3b6d83d
monari1 <francisnyasinga@gmail.com>
2023-12-21 21:15:48 +0300

    final header socials

--- portfolio_final/src/components/header/HeaderSocial.jsx
+++ portfolio_final/src/components/header/HeaderSocial.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+
+
 
 
 const HeaderSocial = () => {
   return (
     <div className='header_socials'>
-        <a href="https://linkedin.com" target='_blank'> <FaLinkedin /></a>
-        <a href="https://github.com" target='_blank'></a>
-        <a href="https://twitter.com" target='_blank'></a>
+        <a href="https://linkedin.com" target= "_blank"> <FaLinkedin /></a>
+        <a href="https://github.com" target='_blank'> <FaGithub /> </a>
+        <a href="https://twitter.com" target='_blank'> <FaSquareXTwitter /></a>
 
 
     </div>
