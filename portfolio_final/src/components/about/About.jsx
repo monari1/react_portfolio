531f16e568973b1151fdd48f1710ced239ab12b3
monari1 <francisnyasinga@gmail.com>
2023-12-22 22:55:32 +0300

    done with jsx for about

--- portfolio_final/src/components/about/About.jsx
+++ portfolio_final/src/components/about/About.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import ME from '../../assets/download.jpeg'
+import ME from '../../assets/programer.jpg'
+import { FiAward } from "react-icons/fi";
+import { CiFolderOn } from "react-icons/ci";
+import { FiUsers } from "react-icons/fi";
+
 
 const About = () => {
   return (
@@ -16,12 +20,35 @@ const About = () => {
         <div className="about_content">
           <div className="about_cards">
             <article className='about_card'>
+            <FiAward className='about_icon' />
               <h5>experiene</h5>
               <small>
                 3+ Years working
               </small>
             </article>
+            <article className='about_card'>
+            <FiUsers className='about_icon' />
+              <h5>Cients</h5>
+              <small>
+                50 clients globally
+              </small>
+            </article>
+            <article className='about_card'>
+            <CiFolderOn className='about_icon' />
+              <h5>Projects</h5>
+              <small>
+                10+ completed projects
+              </small>
+            </article>
           </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Magnam saepe ipsa non veniam vitae a facere, cum assumenda accusamus, 
+             consequatur at? Dolore voluptates placeat eum rerum modi laboriosam dolorem fugiat!
+          </p>
+          <a href="#contacts" className='btn btn-primary'> Let's Talk</a>
+
+
         </div>
       </div>
     </section>
