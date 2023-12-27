f0622c20e011da718842e6ff30bc822418345d45
monari1 <francisnyasinga@gmail.com>
2023-12-27 23:11:45 +0300

    done with portfolio

--- portfolio_final/src/components/testimonials/Testimonials.jsx
+++ portfolio_final/src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import IMAGE from '../../assets/grad2 (1).jpg'
 import './Testimonials.css'
+import { Pagination } from 'swiper/modules';
+
+// import Swiper core and required modules
+import 'swiper/css';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css/pagination';
 
 const data = 
 
@@ -26,23 +35,29 @@ const Testimonials = () => {
     <section>
         <h5>What my Clients Say</h5>
         <h2>Testimonials</h2>
-        <div className="container testimonials_container">
+        <Swiper className="container testimonials_container"
+         modules={[ Pagination]}
+         spaceBetween={40}
+         slidesPerView={1}
+         pagination={{ clickable: true }}
+        
+        >
           {
             data.map(({avatar, name, review}, index)=>{
               return(
-                <article key={index} className='testimonial'>
+                <SwiperSlide key={index} className='testimonial'>
                   <div className="client_avatar">
                     <img src={avatar} alt="" />
                   </div>
                   <h5 className='client_name'> {name}</h5>
                   <small className='client_review'>{review} </small>
-                </article>
+                </SwiperSlide>
               )
 
             })
           }
          
-        </div>
+        </Swiper>
     </section>
 
   
