76feea526b6bf300602bb0de93ca9d7333a4edc7
monari1 <francisnyasinga@gmail.com>
2023-12-27 22:31:41 +0300

    done with reviews

--- portfolio_final/src/components/testimonials/Testimonials.jsx
+++ portfolio_final/src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import IMAGE from '../../assets/grad2 (1).jpg'
+import './Testimonials.css'
 
+const data = 
+
+[ {
+    avatar: IMAGE,
+    name: 'Francis Monari', 
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quo eos, itaque voluptate earum non dignissimos! Perspiciatis iure nulla libero!"
+  },
+  {
+    avatar: IMAGE,
+    name: 'Kimani Mburu', 
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quo eos, itaque voluptate earum non dignissimos! Perspiciatis iure nulla libero!"
+  },
+  {
+    avatar: IMAGE,
+    name: 'Kevin Wamalwa', 
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quo eos, itaque voluptate earum non dignissimos! Perspiciatis iure nulla libero!"
+  },
+
+]
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section>
+        <h5>What my Clients Say</h5>
+        <h2>Testimonials</h2>
+        <div className="container testimonials_container">
+          {
+            data.map(({avatar, name, review}, index)=>{
+              return(
+                <article key={index} className='testimonial'>
+                  <div className="client_avatar">
+                    <img src={avatar} alt="" />
+                  </div>
+                  <h5 className='client_name'> {name}</h5>
+                  <small className='client_review'>{review} </small>
+                </article>
+              )
+
+            })
+          }
+         
+        </div>
+    </section>
+
+  
   )
 }
+
 
 export default Testimonials
