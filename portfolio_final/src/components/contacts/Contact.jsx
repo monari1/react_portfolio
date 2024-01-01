82a87a1799a497b0e647e32ffc5dbfae3e0e6769
monari1 <francisnyasinga@gmail.com>
2024-01-01 22:55:09 +0300

    end of contact jsx

--- portfolio_final/src/components/contacts/Contact.jsx
+++ portfolio_final/src/components/contacts/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Contact.css'
 import { MdOutlineEmail } from "react-icons/md";
 import { FaWhatsapp } from "react-icons/fa6";
 
@@ -16,17 +17,25 @@ const Contact = () => {
           <MdOutlineEmail />
             <h4>Email</h4>
             <h5>francisnyasinga@gmail.com</h5>
-            <a href="mailto:francismonari250@gmail.com">Send a Message</a>
+            <a href="mailto:francismonari250@gmail.com" target='+'>Send a Message</a>
           </article>
           {/* end of email */}
           <article className='contact_option'>
           <FaWhatsapp  />
             <h4>Whatsapp</h4>
             <h5>+254717555587</h5>
-            <a href="https://api.whatsapp.com/send?phone=+254717555587">Send a Message</a>
+            <a href="https://wa.me/+254717555587">Chat with Me</a>
           </article>
        
         </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows = "7" placeholder='Your message'></textarea>
+          <button type='submit' className='btn btn-primary'> Send Message</button>
+=
+
+        </form>
       </div>
     </section>
   )
